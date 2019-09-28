09bf362097d5afc15c44cd6d116ee19edbca484a
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-28 23:56:14 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,12 @@
-call Book
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
   def initialize(title)
     @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 end
